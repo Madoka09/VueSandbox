38b7b1e114e20ba7c4e3dc020dd883ac9a0038fe
Madoka09 <shippuuga.zero@gmail.com>
2020-04-06 17:14:01 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var fieldTest = new Vue({
+    el: '#fieldTest',
+    data: {
+        userInput: '',
+    },
+})
+
+var loopTest = new Vue({
+    el: '#loopTest',
+    data: {
+        loopTest: '',
+    }
+})

823d2b0e8b21974f6e7b44fa2aa5962fad8870cf
Madoka09 <shippuuga.zero@gmail.com>
2020-04-07 17:47:57 -0500

    Completada seccion de Loops

--- index.js
+++ index.js
@@ -3,11 +3,38 @@ var fieldTest = new Vue({
     data: {
         userInput: '',
     },
-})
+});
 
 var loopTest = new Vue({
     el: '#loopTest',
     data: {
-        loopTest: '',
+        inputNumber: '',
+        loopedNumbers: []
+    },
+    methods: {
+        poblateArr: function (event) {
+            this.loopedNumbers.length = 0;
+            for(var i = 1; i <= this.inputNumber; i++){
+                this.loopedNumbers.push({msg: Math.random()});
+            }
+        },
+        clearView: function (event) {
+            this.loopedNumbers = [];
+        }
     }
+});
+
+Vue.component('uikit-card-hover', {
+    data: {},
+    template: '<div class="uk-card uk-card-default uk-card-hover uk-card-body"><h3 class="uk-card-title">Tarjeta de UIKit con Hover</h3></div>'
 })
+
+new Vue({ el: '#cardGenerator'});
+
+
+
+
+
+
+
+
